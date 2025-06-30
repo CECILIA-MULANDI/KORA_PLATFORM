ffaea0528247009b47fd64da8af5f818041ddcdb
CECILIA-MULANDI <mulandicecilia4@gmail.com>
2025-06-30 16:30:23 +0300

    adding login logic

--- kora-insutech-platform/services/controllers/insurance.controller.js
+++ kora-insutech-platform/services/controllers/insurance.controller.js
@@ -1,5 +1,7 @@
 import bcrypt from "bcrypt";
 import db from "../databases/db_connection.js";
+import jwt from "jsonwebtoken";
+import { jwtSecret, jwtExpiration } from "../config/auth.js";
 class InsurerController {
   async registerInsurer(req, res) {
     const { name, email, phone, address, password, confirmPassword } = req.body;
@@ -13,6 +15,15 @@ class InsurerController {
         !confirmPassword
       ) {
         return res.status(400).json({ error: "All fields are required" });
+      }
+      const existingInsurer = await query(
+        "SELECT * FROM insurance-company WHERE email = $1",
+        [email]
+      );
+      if (existingInsurer.rows.length > 0) {
+        return res
+          .status(409)
+          .json({ message: "Insurer with this email already exists." });
       }
 
       if (password !== confirmPassword) {
@@ -31,7 +42,23 @@ class InsurerController {
         `INSERT INTO insurance_company (name, email, phone, address, password) VALUES ($1, $2, $3, $4, $5) RETURNING id, name, email, phone, address,password`,
         [name, email, phone, address, hashedPassword]
       );
-      res.status(201).json({ result });
+      const newInsurer = result.rows[0];
+      const payload = {
+        id: newInsurer.id,
+        email: newInsurer.email,
+      };
+      const token = jwt.sign(payload, jwtSecret, { expiresIn: jwtExpiration });
+      res.status(201).json({
+        message: "Insurance company registered successfuly!",
+        token,
+        user: {
+          id: newInsurer.id,
+          insurance_company_name: newInsurer.name,
+          email: newInsurer.email,
+          phone: newInsurer.phone,
+          address: newInsurer.address,
+        },
+      });
     } catch (e) {
       console.error("Error creating insurance company:", e);
 
@@ -49,12 +76,36 @@ class InsurerController {
       return res.status(400).json({ error: "All fields are required" });
     }
     try {
-      const result = await db.pool.query('SELECT * FROM insurance_company WHERE email = $1', [email])
+      const result = await db.pool.query(
+        "SELECT * FROM insurance_company WHERE email = $1",
+        [email]
+      );
       const insurer = result.rows[0];
-    } catch () {
-      
-    }
+      if (!insurer) {
+        return res.status(401).json({ message: "Invalid credentials." });
+      }
+      const isPasswordValid = await bcrypt.compare(password, insurer.password);
+      if (!isPasswordValid) {
+        return res.status(401).json({ message: "Invalid credentials." });
+      }
+      const payload = {
+        id: newInsurer.id,
+        email: newInsurer.email,
+      };
+      const token = jwt.sign(payload, jwtSecret, { expiresIn: jwtExpiration });
+      res.status(200).json({
+        message: "Login successful!",
+        token,
+        user: {
+          id: newInsurer.id,
 
+          email: newInsurer.email,
+        },
+      });
+    } catch (error) {
+      console.error("Error during login:", error);
+      res.status(500).json({ message: "Server error during login." });
+    }
   }
 }
 export default new InsurerController();
