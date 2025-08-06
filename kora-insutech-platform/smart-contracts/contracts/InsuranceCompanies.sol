010857794cde34259fab902466e8d632e84ddd0b
CECILIA-MULANDI <mulandicecilia4@gmail.com>
2025-08-06 07:28:43 +0300

    adding the iot registration to the contract

--- kora-insutech-platform/smart-contracts/contracts/InsuranceCompanies.sol
+++ kora-insutech-platform/smart-contracts/contracts/InsuranceCompanies.sol
@@ -4,10 +4,13 @@ pragma solidity ^0.8.9;
 contract InsuranceCompanies {
     // Map unique string ID to a boolean indicating registration
     mapping(string => bool) public isInsurerRegistered;
+    mapping(string => bool) public isDeviceRegistered;
+    mapping(string => string) public deviceToPolicy;
     address public owner;
     string[] public insurerIds;
 
     event InsurerRegistered(string indexed _insurerId);
+    event DeviceRegistered(string indexed _deviceId, string indexed _policyId);
 
     constructor() {
         owner = msg.sender;
@@ -34,6 +37,16 @@ contract InsuranceCompanies {
         string calldata _koraInsurerId
     ) public view returns (bool) {
         return isInsurerRegistered[_koraInsurerId];
+    }
+
+    function registerIoTDevice(
+        string calldata _deviceId,
+        string calldata _policyId
+    ) public onlyOwner {
+        require(!isDeviceRegistered[_deviceId], "Device already registered");
+        isDeviceRegistered[_deviceId] = true;
+        deviceToPolicy[_deviceId] = _policyId;
+        emit DeviceRegistered(_deviceId, _policyId);
     }
 
     function getAllInsurerIds() public view returns (string[] memory) {
